5fb2e0dd705501acbd1596c876358049ff167346
Suman Shakya <shakyasuman007@gmail.com>
2022-05-16 17:58:28 +0545

    mongodb atlas updated

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true"
+// const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true"
+const mongoURI = "mongodb+srv://Suman:Crimson%4011@inotebookcluster.vjzwj.mongodb.net/test"
 const connectToMongo =()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log('connected to mongo database');
