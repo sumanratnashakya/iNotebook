e4303c6c64f48047b5fd260b38e735aee47c1567
sumanratnashakya <45494128+sumanratnashakya@users.noreply.github.com>
2022-06-09 17:24:59 +0545

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true"
-// const mongoURI = "mongodb+srv://Suman:Crimson%4011@inotebookcluster.vjzwj.mongodb.net/test"
+// const mongoURI = "mongodb+srv://Suman:helloworld%4011@inotebookcluster.vjzwj.mongodb.net/test"
 const mongoURI = "https://data.mongodb-api.com/app/data-eqghd/endpoint/data/beta"
 const connectToMongo =()=>{
     mongoose.connect(mongoURI, ()=>{
